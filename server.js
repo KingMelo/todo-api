fc7dd09db6dbe3892a707c02823c96a67ccb5f90
King Penson II <hello@kingpenson.com>
2018-04-24 23:03:33 -0500

    refactored POST body object

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
@@ -21,15 +23,8 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
-    
     //Iterate of todos array. Find the match.
-    
-    todos.forEach(function(todo){
-        if(todo.id == todoId){
-           matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId});
     
     if (matchedTodo) {
         res.json(matchedTodo);
@@ -40,7 +35,16 @@ app.get('/todos/:id', function(req, res){
 
 // POST /todos
 app.post('/todos', function(req, res){
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed'); //Use _.pick to only pick description and completed 
+    
+    
+    
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+    
+    // set body.description to be trimmed value
+    body.description = body.description.trim();
     
     //add id field
     body.id = todoNextId++;    
