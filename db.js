0f15766bb1d97b540c7e8c78cc1ac09f3342378f
KingMelo <hello@kingpenson.com>
2018-05-03 11:09:30 -0500

    added uri for postgres to db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if(env === 'production'){
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
+    sequelize = new Sequelize('postgres://hdcairhigejlyw:173af4fb2df7447b9dfb1ec8695c15e59a6d9e246eeed65c060bb84c3bbe8e41@ec2-54-24', {
         dialect: 'postgres'
     });
 } else {
